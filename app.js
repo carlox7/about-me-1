4c6e345cb132f41b9c3519c1f70f1a8d4ed1892c
Kevin Schmidt <NSPtuning@gmail.com>
2017-02-10 14:01:04 -0800

    re-commit app.js

---
+++ app.js
@@ -0,0 +1,140 @@
+'use strict';
+
+// Quiz Confirmation
+var confirmQuiz = prompt('Hello, would you like to take a quiz? Answer using Y or N').toUpperCase();
+
+if(confirmQuiz === 'Y' || confirmQuiz === 'YES'){
+  console.log('Awesome! Letsssa Goooooo!');
+} else {
+  console.log('Your taking it anyway.  Email StopCryingandTaketheQuiz@aol.com with complaints. Thx');
+}
+
+var QuestionAnswers = (quizResponseOne === 'Y' || quizResponseOne === 'YES');
+
+// Question One
+var quizResponseOne = prompt('Does Kevin have a cat?').toUpperCase();
+
+if(quizResponseOne === QuestionAnswers{
+  console.log('Awesome! You are correct!');
+} else if(quizResponseOne === 'N' || quizResponseOne === 'NO'){
+  console.log('No!. Kevin has a dog. Cats are the devil!');
+} else {
+  console.log('Please answer using Y or N or Yes/No. PAY ATTENTION');
+}
+
+// // Question Two
+// var quizResponseTwo = prompt('Has Kevin been to Spain?').toUpperCase();
+//
+// if(quizResponseTwo === 'Y' || quizResponseTwo === 'YES'){
+//   console.log('Correct, Kevin has been to many European countries but not Spain. He would love to go someday!');
+// } else if(quizResponseTwo === 'N' || quizResponseTwo === 'NO'){
+//   console.log('Your right! Kevin has gone to many other European countries but never Spain... yet');
+// } else {
+//   console.log('Please answer using Y/N or Yes/No. Didn\'t we go over this?');
+// }
+//
+// // Question Three
+// var quizResponseThree = prompt('Has Kevin ever gone 180mph?').toUpperCase();
+//
+// if(quizResponseThree === 'Y' || quizResponseThree === 'YES'){
+//   console.log('Yes! 181mph in an Audi S4 with BIG turbos on it. Dont tell the Po-Po.');
+// } else if(quizResponseThree === 'N' || quizResponseThree === 'NO'){
+// } else {
+//   console.log('Please answer using Y/N or Yes/No. I feel like I\'m repeating myself.');
+// }
+//
+// // Question Four
+// var quizResponseFour = prompt('Is lager Kevin\'s favorite kind of beer?').toUpperCase();
+//
+// if(quizResponseFour === 'Y' || quizResponseFour === 'YES'){
+//   console.log('Nope, he likes himself a nice cold IPA.');
+// } else if(quizResponseFour === 'N' || quizResponseFour === 'NO'){
+//   console.log('Your right! You won a free IPA.');
+// } else {
+//   console.log('Please answer using Y/N or Yes/No. Not trying to insult your intelligence but....');
+// }
+//
+// // Question Five
+// var quizResponseFive = prompt('Can Kevin do a jump kick?').toUpperCase();
+//
+// if(quizResponseFive === 'Y' || quizResponseFive === 'YES'){
+//   console.log('Your confidence is appreciated, but Kevin would hurt himself. He is getting old.');
+// } else if(quizResponseFive === 'N' || quizResponseFive === 'NO'){
+//   console.log('What? He can do a jump kick like a friggin ninja!');
+// } else {
+//   console.log('Please answer using Y/N or Yes/No. Just do it. It will make everyone\'s life easier.');
+// }
+//
+// // Question Six Random Number
+// var correctNumber = 14;
+//
+// for (var quizOne = 0; quizOne < 4; quizOne++){
+//   var quizResponseSix = Number(prompt('Bonus Round! Guess a number between 1 and 10'));
+//   // console.log(typeof quizResponseSix);
+//   // console.log('going through 4 loop');
+//   if (quizResponseSix === correctAnswer){
+//     alert('You guessed correct!');
+//     break;
+//   } else if (quizResponseSix < correctAnswer){
+//     alert('Too low! Guess again.');
+//   } else if (quizResponseSix > correctAnswer){
+//     alert('Too high!');
+//   }
+// }
+
+// var correctState = ['WASHINGTON, OREGON, IDAHO, MONTANA, NEW YORK, NEW JERSEY, TEXAS, NEVADA, MONTANA, CALIFORNIA, PENNSYLVANIA, NORTH CAROLINA'];
+//
+// // console.log(correctState);
+//
+// // for (var questionSix = 0; questionSix < 4; questionSix++){
+// //   var quizResponseSeven = (prompt('What US states has Kevin been to?').toUpperCase);
+// //   if (correctState === ()){
+// //     prompt('Thats 1, try to pick another');
+// //   } else if (quizResponseSeven < correctState){
+// //     alert('Too low! Guess again.');
+// //   } else if (quizResponseSeven > correctState){
+// //     alert('Too high!');
+// //   }
+// // }
+// var confirmQuiz = confirm('Now your going to take a quiz. Now.')
+//
+// var score = 0
+//
+// var StartQuiz = false;
+//
+// console.console.log();
+//
+// do {
+//   if (confirmQuiz) {
+//     prompt('Slright, lets go');
+//   } else {
+//   confirmQuiz = alert('wrong answer')
+//   }
+// } while (true);
+//
+//
+// // array = array("Kyle","Ben","Sue","Phil","Ben","Mary","Sue","Ben");
+// // Counts = array_count_values($array);
+// // echo $counts['Ben'];
+//
+// // var arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+// // var counts = {};
+// //
+// // for(var i = 0; i< arr.length; i++) {
+// //     var num = arr[i];
+// //     counts[num] = counts[num] ? counts[num]+1 : 1;
+// break
+//
+// sumOfnumbers(3,7);
+//
+// var name = 'Adam';
+// var otherName = 'Amanda';
+// var myDog = 'Mika';
+//
+// var addTwoThings = function(numOne, numTwo){
+//   return numOne + numTwo;
+// };
+//
+// function sumOfnumbers(numOne, numTwo){
+//   console.log(numOne + numTwo);
+// }
