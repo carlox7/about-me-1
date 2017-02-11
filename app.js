9adfdc5d3ca9667dae52615b414d18b7de7e5907
Carlo Santos <carlox7@gmail.com>
2017-02-10 17:07:23 -0800

    finished functions and fixed last question

--- app.js
+++ app.js
@@ -24,75 +24,96 @@ catQuestion();
 
 function spainQuestion(){
 //Question Two
-var quizResponseTwo = prompt('Has Kevin been to Spain?').toUpperCase();
+  var quizResponseTwo = prompt('Has Kevin been to Spain?').toUpperCase();
 
-if(quizResponseTwo === 'Y' || quizResponseTwo === 'YES'){
-  console.log('Correct, Kevin has been to many European countries but not Spain. He would love to go someday!');
-} else if(quizResponseTwo === 'N' || quizResponseTwo === 'NO'){
-  console.log('Your right! Kevin has gone to many other European countries but never Spain... yet');
-} else {
-  console.log('Please answer using Y/N or Yes/No. Didn\'t we go over this?');
-}
+  if(quizResponseTwo === 'Y' || quizResponseTwo === 'YES'){
+    console.log('Correct, Kevin has been to many European countries but not Spain. He would love to go someday!');
+  } else if(quizResponseTwo === 'N' || quizResponseTwo === 'NO'){
+    console.log('Your right! Kevin has gone to many other European countries but never Spain... yet');
+  } else {
+    console.log('Please answer using Y/N or Yes/No. Didn\'t we go over this?');
+  }
 }
 spainQuestion();
 
 function mphQuestion(){
 // Question Three
-var quizResponseThree = prompt('Has Kevin ever gone 180mph?').toUpperCase();
+  var quizResponseThree = prompt('Has Kevin ever gone 180mph?').toUpperCase();
 
-if(quizResponseThree === 'Y' || quizResponseThree === 'YES'){
-  console.log('Yes! 181mph in an Audi S4 with BIG turbos on it. Dont tell the Po-Po.');
-} else if(quizResponseThree === 'N' || quizResponseThree === 'NO'){
-} else {
-  console.log('Please answer using Y/N or Yes/No. I feel like I\'m repeating myself.');
-}
+  if(quizResponseThree === 'Y' || quizResponseThree === 'YES'){
+    console.log('Yes! 181mph in an Audi S4 with BIG turbos on it. Dont tell the Po-Po.');
+  } else if(quizResponseThree === 'N' || quizResponseThree === 'NO'){
+  } else {
+    console.log('Please answer using Y/N or Yes/No. I feel like I\'m repeating myself.');
+  }
 }
 mphQuestion();
 
 function beerQuestion(){
 // Question Four
-var quizResponseFour = prompt('Is lager Kevin\'s favorite kind of beer?').toUpperCase();
+  var quizResponseFour = prompt('Is lager Kevin\'s favorite kind of beer?').toUpperCase();
 
-if(quizResponseFour === 'Y' || quizResponseFour === 'YES'){
-  console.log('Nope, he likes himself a nice cold IPA.');
-} else if(quizResponseFour === 'N' || quizResponseFour === 'NO'){
-  console.log('Your right! You won a free IPA.');
-} else {
-  console.log('Please answer using Y/N or Yes/No. Not trying to insult your intelligence but....');
-}
+  if(quizResponseFour === 'Y' || quizResponseFour === 'YES'){
+    console.log('Nope, he likes himself a nice cold IPA.');
+  } else if(quizResponseFour === 'N' || quizResponseFour === 'NO'){
+    console.log('Your right! You won a free IPA.');
+  } else {
+    console.log('Please answer using Y/N or Yes/No. Not trying to insult your intelligence but....');
+  }
 }
 beerQuestion();
 
 function kickQuestion(){
 // Question Five
-var quizResponseFive = prompt('Can Kevin do a jump kick?').toUpperCase();
+  var quizResponseFive = prompt('Can Kevin do a jump kick?').toUpperCase();
 
-if(quizResponseFive === 'Y' || quizResponseFive === 'YES'){
-  console.log('Your confidence is appreciated, but Kevin would hurt himself. He is getting old.');
-} else if(quizResponseFive === 'N' || quizResponseFive === 'NO'){
-  console.log('What? He can do a jump kick like a friggin ninja!');
-} else {
-  console.log('Please answer using Y/N or Yes/No. Just do it. It will make everyone\'s life easier.');
-}
-}
-kickQuestion();
-// Question Six Random Number
-var correctNumber = 14;
-
-for (var quizOne = 0; quizOne < 4; quizOne++){
-  var quizResponseSix = Number(prompt('Bonus Round! Guess a number between 1 and 10'));
-  // console.log(typeof quizResponseSix);
-  // console.log('going through 4 loop');
-  if (quizResponseSix === correctAnswer){
-    alert('You guessed correct!');
-    break;
-  } else if (quizResponseSix < correctAnswer){
-    alert('Too low! Guess again.');
-  } else if (quizResponseSix > correctAnswer){
-    alert('Too high!');
+  if(quizResponseFive === 'Y' || quizResponseFive === 'YES'){
+    console.log('Your confidence is appreciated, but Kevin would hurt himself. He is getting old.');
+  } else if(quizResponseFive === 'N' || quizResponseFive === 'NO'){
+    console.log('What? He can do a jump kick like a friggin ninja!');
+  } else {
+    console.log('Please answer using Y/N or Yes/No. Just do it. It will make everyone\'s life easier.');
   }
 }
+kickQuestion();
 
+function numGame(){
+// Question Six Random Number
+  var correctAnswer = 6;
+
+  for (var i = 0; i < 4; i++){
+    var quizResponseSix = prompt('Bonus Round! Guess a number between 1 and 10');
+    var numberGuess = parseInt(quizResponseSix);
+    if (numberGuess === correctAnswer){
+      alert('You guessed correct!');
+      break;
+    } else if (numberGuess < correctAnswer){
+      alert('Too low! Guess again.');
+    } else if (numberGuess > correctAnswer){
+      alert('Too high!');
+    }
+  }
+}
+numGame();
+
+function statesQuestion(){
+//Question Seven States Question
+  var statesVisited = ['WASHINGTON', 'OREGON'];
+//number of guesses
+  for(var i = 0; i < 6; i++){
+    var quizResponseSeven = prompt('Which states has Kevin visited?').toUpperCase();
+    for(var ii = 0; ii < statesVisited.length; ii++){
+      if(quizResponseSeven === statesVisited[ii]);
+      alert('Good Job! ' + ii + ' is a state Kevin has visited');
+      i = 30;
+      //rightAnswer++;
+      break;
+    }
+  } if(i === 5){
+    alert('Your all out of guesses. The correct answers are ' + statesVisited);
+  }
+}
+statesQuestion();
 // var correctState = ['WASHINGTON, OREGON, IDAHO, MONTANA, NEW YORK, NEW JERSEY, TEXAS, NEVADA, MONTANA, CALIFORNIA, PENNSYLVANIA, NORTH CAROLINA'];
 //
 // // console.log(correctState);
